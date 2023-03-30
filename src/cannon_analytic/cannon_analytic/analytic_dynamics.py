62ee433369c80a3e3dad5a916270c2d692e0d20e
shalev <shalev@lulav.space>
2023-03-30 15:52:58 +0300

    separated cannon module from ROS node

--- src/cannon_analytic/cannon_analytic/analytic_dynamics.py
+++ src/cannon_analytic/cannon_analytic/analytic_dynamics.py
@@ -1,87 +1,52 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray, Bool
-import math
+from cannon_analytic.cannon import cannon
 
 class analytic_dynamics(Node):
 
 	def __init__(self):
 		super().__init__('cannon_dynamics')
 
-		self.cannon_default_data()
-		self.init_calculations()
-
-		self.publisher_ = self.create_publisher(Float64MultiArray, 'cannon/state', 10)
-		self.sub_scheduler = self.create_subscription(Bool, '/scheduler', self.timer_callback, 1)
-
-		# for a stand-alone version of this node (which is not dependant on a scheduler node),
-		# comment out the previous line and uncomment the following two lines.
-		#timer_period = 0.01  # seconds
-		#self.timer = self.create_timer(timer_period, self.timer_callback)
-
-
-	def cannon_default_data(self):
-		self.pos_orig = [0.0, 0.0]
-		self.pos = [0.0, 0.0]
-		self.acc = [0.0, -9.81]
-		self.time = 0.0
-		self.impact = False
-		self.impactTime = 0.0
+		# declare and init cannon model
+		self.cann = cannon(self.get_logger())
 
 		# declare parameters
 		self.declare_parameter('init_speed', 50.0)
 		self.declare_parameter('init_angle', 30.0)
 
 		# get value from yaml
-		self.init_speed = self.get_parameter('init_speed').get_parameter_value().double_value
-		self.init_angle = self.get_parameter('init_angle').get_parameter_value().double_value
+		self.cann.init_speed = self.get_parameter('init_speed').get_parameter_value().double_value
+		self.cann.init_angle = self.get_parameter('init_angle').get_parameter_value().double_value
 
+		# publish and subscribe
+		self.publisher_ = self.create_publisher(Float64MultiArray, 'cannon/state', 10)
+		self.sub_scheduler = self.create_subscription(Bool, '/scheduler', self.timer_callback, 1)
 
-	def init_calculations(self):
-		self.vel_orig = [math.cos(math.radians(self.init_angle))*self.init_speed, 
-						 math.sin(math.radians(self.init_angle))*self.init_speed]
-		self.vel = [self.vel_orig[0], self.vel_orig[1]]
+		# for a stand-alone version of this node (which is not dependant on a scheduler node):
+		# - comment out the previous line 
+		# - uncomment the following two lines
+		# - remove the heartbeat parameter from timer_callback
+		#timer_period = 0.01  # seconds
+		#self.timer = self.create_timer(timer_period, self.timer_callback)
 
-
-	def analytic_step(self):
-		self.vel[0] = self.vel_orig[0] + self.acc[0] * self.time
-		self.vel[1] = self.vel_orig[1] + self.acc[1] * self.time
-
-		self.pos[0] = self.pos_orig[0] + (self.vel_orig[0] + (0.5) * self.acc[0] * self.time) * self.time
-		self.pos[1] = self.pos_orig[1] + (self.vel_orig[1] + (0.5) * self.acc[1] * self.time) * self.time
-		
-		if (self.pos[1] < 0.0):
-			self.impactTime = (-self.vel_orig[1] - math.sqrt(self.vel_orig[1] * self.vel_orig[1] - 2 * self.pos_orig[1]))/self.acc[1]
-			self.pos[0] = self.impactTime * self.vel_orig[0]
-			self.pos[1] = 0.0
-			self.vel[0] = 0.0
-			self.vel[1] = 0.0
-
-			if not self.impact:
-				self.impact = True
-				self.get_logger().info(f"\n\nIMPACT: t = {self.impactTime}, pos[0] = {self.pos[0]}\n\n")
-        
-		# Increment time by the time delta that matches the frequency 
-		# specified in the scheduler node.
-		self.time += 0.01
-		
 
 	def timer_callback(self,heartbeat):
 		msg = Float64MultiArray()
 
 		# single calculation step 
-		self.analytic_step()
+		self.cann.analytic_step()
 
-		msg.data = [self.pos[0], 
-					self.pos[1], 
-					self.vel[0], 
-					self.vel[1]]
+		msg.data = [self.cann.pos[0], 
+			    self.cann.pos[1], 
+			    self.cann.vel[0], 
+			    self.cann.vel[1]]
 
 		# publish result
 		self.publisher_.publish(msg)
 
 		# for debugging: print published data in console
-		if not self.impact:
+		if not self.cann.impact:
 			self.get_logger().info(f"Publishing: {msg.data}")
 
 
